e4696654638c63e44325555d096e2b39c4e5d69b
BosEriko <bos@eriko.ph>
2026-04-28 18:22:11 +0800

    Feature: Add password to create DTO of user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
@@ -7,4 +7,8 @@ export class CreateUserDto {
   @IsOptional()
   @IsString()
   name?: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
 }
